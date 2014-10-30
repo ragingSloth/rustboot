b9a02fe1017a8e53b49ff5686659921d4ae0897c
ezra <ezra@indico.io>
2014-10-30 02:38:19 -0400

    gdt setup

--- loader.asm
+++ loader.asm
@@ -2,89 +2,43 @@ use16
 
 org 0x7c00
 
-boot:
-    ; initialize segment registers
+loadSegments:
     xor ax, ax
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
-    ; initialize stack
-    mov sp, 0x7bfe
-    ; load rust code into 0x7e00 so we can jump to it later
-    mov ah, 2       ; read
-    mov al, 24      ; 24 sectors (12 KiB)
-    mov ch, 0       ; cylinder & 0xff
-    mov cl, 2       ; sector | ((cylinder >> 2) & 0xc0)
-    mov dh, 0       ; head
-    mov bx, 0x7e00  ; read buffer
-    int 0x13
-    jc error
-    ; load protected mode GDT and a null IDT (we don't need interrupts)
-    cli
-    lgdt [gdtr]
-    lidt [idtr]
-    ; set protected mode bit of cr0
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
-    ; far jump to load CS with 32 bit segment
-    jmp 0x08:protected_mode
-
-error:
-    mov si, .msg
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0e
-    int 0x10
-    jmp .loop
-.done:
-    jmp $
-    .msg db "could not read disk", 0
-
-protected_mode:
-    use32
-    ; load all the other segments with 32 bit data segments
-    mov eax, 0x10
-    mov ds, eax
-    mov es, eax
-    mov fs, eax
-    mov gs, eax
-    mov ss, eax
-    ; set up stack
-    mov esp, 0x7bfc
-    ; jump into rust
-    call 0x7e00
-    jmp $
+    lgdt gdtr
 
 gdtr:
-    dw (gdt_end - gdt) + 1  ; size
-    dd gdt                  ; offset
-
-idtr:
-    dw 0
-    dd 0
-
+    dw (gdt_end - gdt) + 1
+    db gdt
 gdt:
-    ; null entry
-    dq 0
-    ; code entry
-    dw 0xffff       ; limit 0:15
-    dw 0x0000       ; base 0:15
-    db 0x00         ; base 16:23
-    db 0b10011010   ; access byte - code
-    db 0x4f         ; flags/(limit 16:19). flag is set to 32 bit protected mode
-    db 0x00         ; base 24:31
-    ; data entry
-    dw 0xffff       ; limit 0:15
-    dw 0x0000       ; base 0:15
-    db 0x00         ; base 16:23
-    db 0b10010010   ; access byte - data
-    db 0x4f         ; flags/(limit 16:19). flag is set to 32 bit protected mode
-    db 0x00         ; base 24:31
-gdt_end:
-
-times 510-($-$$) db 0
-db 0x55
-db 0xaa
+    dq 0 ;now I can segfault
+    ;code segment
+    dw 0xFFFF;max addressable soace b0-16
+    dw 0x0000;base unknown
+    db 0x00;more base
+    db 0b10011010;access byte 
+        ;present
+        ;2bit ring 0
+        ;1
+        ;executable = true
+        ;grow up, eg a +1 = 0xe ->oxf
+        ;readable
+        ;cpu sets this
+    db 0b01001111;limit/settings byte
+        ;limit units 0 for bytes which matches 0xfffff
+        ;size of protected, 1 for 32 bit
+        ;2 0s
+        ;0xf gets tacked onto end of limit
+    dw 0x00 ; end of base
+    ;data segment
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0b10010010
+    db 0b01001111
+    dw 0x00 
