2704fbc9fb5bf9e1f6f827a21deb2709fae3fd60
ezra <ezra@indico.io>
2014-11-19 22:30:37 -0500

    rewrote loader

--- loader.asm
+++ loader.asm
@@ -4,52 +4,43 @@ org 0x7c00
 
 clear_registers:
     xor ax, ax
-    mov ax, 0x10
+    mov bx, ax
     mov ds, ax
     mov es, ax
-    mov fs, ax
-    mov gs, ax
     mov ss, ax
    
-    mov ah, 9           ; Print "===="
-    mov al, '='         ;
-    mov bx, 7           ;
-    mov cx, 4           ;
-    int 10h             ;
-
-    hang:                       ; Hang!
-        jmp hang 
+    mov ah, 2 ; read
+    mov al, 24 ; 24 sectors (12 KiB)
+    mov ch, 0 ; cylinder & 0xff
+    mov cl, 2 ; sector | ((cylinder >> 2) & 0xc0)
+    mov dh, 0 ; head
+    mov bx, 0x7e00
+    int 0x13
+    xor ax, ax
 
     cli
     lgdt [gdtr]
     mov eax, cr0
     or eax, 1
     mov cr0, eax
-    jmp 0x7e00
+    jmp 0x08:protected
 
-error:
-    mov si, .msg
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0e
-    int 0x10
-    jmp .loop
-.done:
+protected:
+    use32
+    mov esp, 0x7bfc
+    call 0x7e00
     jmp $
-    .msg db "could not read disk", 0
 
 gdtr:
     dw (gdt_end - gdt) + 1
-    db gdt
+    dd gdt
 gdt:
     dq 0 ;now I can segfault
     ;code segment
-    dw 0xFFFF;max addressable soace b0-16
+    dw 0xffff;max addressable soace b0-16
     dw 0x0000;base unknown
     db 0x00;more base
-    db 0b10011010;access byte 
+    db 0b10011010
         ;present
         ;2bit ring 0
         ;1
@@ -62,15 +53,15 @@ gdt:
         ;size of protected, 1 for 32 bit
         ;2 0s
         ;0xf gets tacked onto end of limit
-    dw 0x00 ; end of base
+    db 0x00 
     ;data segment
-    dw 0xFFFF
+    dw 0xffff
     dw 0x0000
     db 0x00
     db 0b10010010
     db 0b01001111
-    dw 0x00 
+    db 0x00 
 gdt_end:
 
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0xaa55
