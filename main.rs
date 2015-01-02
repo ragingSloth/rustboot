b081929602149161a0dd0e05d05bc405d128ab90
ezra <ezra@indico.io>
2015-01-02 16:27:32 -0500

    global offset table

--- main.rs
+++ main.rs
@@ -5,13 +5,6 @@
 #![feature(intrinsics)]
 
 
-#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
-#[lang = "eh_personality"] extern fn eh_personality() {}
-#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {}  }
-#[lang="sized"]
-pub trait Sized for Sized? {}
-#[lang="copy"]
-pub trait Copy for Sized? {}
 
 pub mod utils;
 pub mod io;
@@ -20,12 +13,13 @@ pub mod io;
 #[no_stack_check]
 pub extern "C" fn main() {
     io::clear_screen(io::Black);
-    let ref mut x = io::Cell {
+    let mut x = io::Cell {
         x : 1,
         y : 0,
         bg : io::Black as u16,
         fg : io::White as u16, 
     };
-    io::put_char(x, 'f');
-    //io::put(&mut x, "hello world!");
+    io::put_char(&x, 'f');
+    io::puts(&mut x, "hello world!");
+    //io::r_write((x as *mut io::Cell), unsafe{utils::transmute("hello world!")});
 }
